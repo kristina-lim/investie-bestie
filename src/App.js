076b85b35a94e1a3132438bae32c48ac58f73ccf
kristina-lim <kristinahlim@gmail.com>
2024-11-29 23:29:21 -0800

    Add components file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import StockRow from './components/StockRow.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <table className="table mt-5">
+          <thead>
+            <tr>
+              <th>Ticker</th>
+              <th>Price</th>
+              <th>Date</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          {/* top down */}
+          <tbody>
+            {/* StockRow component and ticker attribute */}
+            <StockRow ticker="aapl" />
+            <StockRow ticker="goog" />
+            <StockRow ticker="msft" />
+            <StockRow ticker="tsla" />
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/components/StockRow.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class StockRow extends Component {
+
+  render() {
+    return (
+      <tr>
+        <td>{this.props.ticker}</td>
+        <td>asdsd</td>
+        <td>asdasd</td>
+        <td>asdasd</td>
+      </tr>
+    )
+  }
+}
+
+export default StockRow;
